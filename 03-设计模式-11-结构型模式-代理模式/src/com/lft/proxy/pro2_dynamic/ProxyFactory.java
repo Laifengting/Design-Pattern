d51079d49465dbeca4e6f67894819eb4d535365e
laifengting <laifengting@foxmail.com>
2021-05-08 09:28:56 +0800

    修改类图样式

--- 03-设计模式-11-结构型模式-代理模式/src/com/lft/proxy/pro2_dynamic/ProxyFactory.java
+++ 03-设计模式-11-结构型模式-代理模式/src/com/lft/proxy/pro2_dynamic/ProxyFactory.java
@@ -38,23 +38,25 @@ public class ProxyFactory {
 		 * 说明：
 		 * @param loader 指定当前目标对象使用的类加载器，获取加载器的方法固定
 		 * @param interfaces 目标对象实现的接口类型，使用泛型方式确认类型
-		 * @param h    事件处理，执行目标对象的方法时，会触发事件处理器方法，会把当前进程句柄执行的目标对象方法作为参数传入。
+		 * @param h    调用处理器，事件处理，执行目标对象的方法时，会触发事件处理器方法，会把当前进程句柄执行的目标对象方法作为参数传入。
 		 * @return
-		 * public static Object newProxyInstanc e(ClassLoader loader, Clas s < ? >[] interfaces, InvocationHandler h)
+		 * public static Object newProxyInstanc e(ClassLoader loader, Class<?>[] interfaces, InvocationHandler h)
 		 */
-		return Proxy.newProxyInstance(target.getClass().getClassLoader(), target.getClass().getInterfaces(),
-									  new InvocationHandler() {
-										  @Override
-										  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-											  System.out.println("JDK代理开始...");
-				
-											  // 通过反射机制调用目标对象的方法。
-											  Object returnValue = method.invoke(target, args);
-				
-											  System.out.println("JDK代理提交...");
-											  return returnValue;
-										  }
-									  });
+		return Proxy.newProxyInstance(
+				target.getClass().getClassLoader(),
+				target.getClass().getInterfaces(),
+				new InvocationHandler() {
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						System.out.println("JDK代理开始...");
+						
+						// 通过反射机制调用目标对象的方法。
+						Object returnValue = method.invoke(target, args);
+						
+						System.out.println("JDK代理提交...");
+						return returnValue;
+					}
+				});
 	}
 	
 }
